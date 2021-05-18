31a16f69f9cf603e6f2289f7ab5a81d4c912aa34
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-05-17 18:44:42 -0700

    Forms

---
+++ src/SingIn.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import SignIn from "./SignIn";
+import { render, fireEvent, getByLabelText } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import { ExpansionPanelActions } from "@material-ui/core";
+
+describe("SignIn", () => {
+  describe("with valid inputs", () => {
+    it("calls onSubmit function", async () => {
+      const mockOnSubmit = jest.fn();
+      const { getByLabelText, getByRole } = render(
+        <SignIn onSubmit={mockOnSubmit} />
+      );
+      await act(async () => {
+        fireEvent.change(getByLabelText("Email Address *"), {
+          target: { value: "email@test.com" },
+        });
+        fireEvent.change(getByLabelText("Password *"), {
+          target: { value: "1234567" },
+        });
+      });
+      await act(async () => {
+        fireEvent.click(getByRole("button"));
+      });
+      expect(mockOnSubmit).toHaveBeenCalled();
+    });
+  });
+
+  describe("with invalid email", () => {
+    it("renders email validation error", async () => {
+      const { getByLabelText, container } = render(<SignIn />);
+      await act(async () => {
+        const emailInput = getByLabelText("Email Address *");
+        fireEvent.change(emailInput, { target: { value: "invalid email" } });
+        fireEvent.blur(emailInput);
+      });
+
+      expect(container.innerHTML).toMatch("Enter a valid email");
+    });
+  });
+  describe("with invalid password", () => {
+    it("renders te pssword validation error", async () => {
+      const { getByLabelText, container } = render(<SignIn />);
+      await act(async () => {
+        const passwordInput = getByLabelText("Password *");
+        fireEvent.change(passwordInput, { target: { value: "123" } });
+        fireEvent.blur(passwordInput);
+      });
+
+      expect(container.innerHTML).toMatch(
+        "Password should be longer than 6 characters"
+      );
+    });
+  });
+});
